ef66e777cf8f72bf4df639ce18737158e349871a
yhoa0008 <yurihoashi@gmail.com>
2025-07-15 14:39:41 +1000

    Create filter_data.py
    objective description and steps

---
+++ python-emotiv/filter_data.py
@@ -0,0 +1,17 @@
+'''
+
+Objective:
+To isolate the EEG signal components related to cognitive processes (such as attention, focus, and thinking) 
+by removing noise and filtering the signal to retain only the relevant brainwave frequencies.
+
+'''
+
+
+# Band-Pass Filter Design
+
+
+# Fourier Transform for Frequency Analysis
+
+
+
+
